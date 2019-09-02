8b47c8b80f3b6e167490f9cc7a893734d1e0fdfd
threelittlemonkeys <sjlee7642@gmail.com>
2019-09-02 14:03:03 +0900

    Update by commit

--- parameters.py
+++ parameters.py
@@ -14,8 +14,8 @@ EMBED = {"lookup": 300} # embeddings (char-cnn, char-rnn, lookup, sae)
 HIDDEN_SIZE = 1000
 DROPOUT = 0.5
 LEARNING_RATE = 1e-4
-BEAM_SIZE = 1
-VERBOSE = 2 # 0: None, 1: attention heatmap, 2: beam search
+BEAM_SIZE = 3
+VERBOSE = 0 # 0: None, 1: attention heatmap, 2: beam search
 EVAL_EVERY = 10
 SAVE_EVERY = 10
 
